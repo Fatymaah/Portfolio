6a68ac5f2a6d70d0cdee9a6a5857e58da10194b5
Alchemy <maanoatu17@gmail.com>
2018-11-24 22:02:04 +0300

    finished overlay

--- dist/portfolio.js
+++ dist/portfolio.js
@@ -4,6 +4,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu  = document.querySelector('.menu');
 const menuNav  = document.querySelector('.menu-nav');
+const menuBranding  = document.querySelector('.menu-branding');
 const navItems  = document.querySelectorAll('.nav-item');
 
 
@@ -16,6 +17,7 @@ function toggleMenu(){
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
+        menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
         showMenu = true;
@@ -27,6 +29,7 @@ function toggleMenu(){
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
         showMenu = false;
